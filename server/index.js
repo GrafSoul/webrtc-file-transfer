5bc5782843eb82b5cfbdefcdadcbd737811ea9c8
GrafSoul <grafsoul@gmail.com>
2020-07-30 12:30:15 +0500

    feat: Changed env PROD for Server.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const socketToRoom = {};
 
 console.log(process.env.PROD);
 
-if (!process.env.PROD) {
+if (process.env.PROD) {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
